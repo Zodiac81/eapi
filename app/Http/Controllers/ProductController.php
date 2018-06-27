1266e5b18f452538edaece42a6322c2ab2549bb7
Aleksey <aleksey.derevyanko81@gmail.com>
2018-06-27 10:42:56 +0300

    "27.06 Product collection resource done"

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
 use Illuminate\Http\Request;
@@ -17,7 +18,7 @@ class ProductController extends Controller
     public function index()
     {
 
-        return Product::all();
+        return ProductCollection::collection(Product::all());
     }
 
     /**
@@ -49,8 +50,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-//        return $product;
-//        exit;
+
         return new ProductResource($product);
     }
 
